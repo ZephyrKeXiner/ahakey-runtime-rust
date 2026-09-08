bba369c21b5fc29cb38d82356f1d5648a0826f95
sakruhnab1 <91109111+ZephyrKeXiner@users.noreply.github.com>
2026-09-08 17:11:24 +0800

    feat: add bluetooth library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use btleplug::api::bleuuid::uuid_from_u16;
+use tokio::time::{sleep, Duration};
+use btleplug::platform::{Adapter, Manager, Peripheral};
+use btleplug::api::{self, Central, Manager as _, Peripheral as _, RetrievePeripheralsOptions, ScanFilter, CharPropFlags, WriteType};
+use futures_util::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let manager = Manager::new().await?;
+    
+    let adapter  = manager.adapters().await?;
+    let central = adapter.into_iter().nth(0).expect("No Bluetooth adapters found");
+
+    let devices = central.retrieve_peripherals(
+        RetrievePeripheralsOptions {
+            identifiers: None, 
+            services: Some(vec![
+                uuid_from_u16(0x7340),
+                uuid_from_u16(0x1812),
+            ])
+        }
+    ).await?;
+
+    for peripheral in devices {
+        let Some(props) = peripheral.properties().await? else {
+            continue;
+        }; 
+    }
+
+
+
+    Ok(())
 }
