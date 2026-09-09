69e947fc8f65afc43a2a4e86e55052956875b25f
sakruhnab1 <91109111+ZephyrKeXiner@users.noreply.github.com>
2026-09-09 17:28:05 +0800

    tests: add println

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::os::unix::process;
 
 use btleplug::api::bleuuid::uuid_from_u16;
 use btleplug::platform::{Manager, Peripheral};
@@ -32,8 +33,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         if !lower.starts_with("ahakey") && !lower.starts_with("vibe code") {
             continue;
         }
+        
+        println!("{:#?}", props);
     }
-    
 
     Ok(())
 }
